d186c9aff554a4cb315a4394659c667e61fdaff3
AndersonHaah <Andersonhaah1@gmail.com>
2025-08-26 07:28:50 -0300

    AS

--- frontend/src/components/assets/imgs/projectsIMG/ProjectsImgs.jsx
+++ frontend/src/components/assets/imgs/projectsIMG/ProjectsImgs.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import PI1 from './portifolio-gif.gif'
 import PI2 from './PI2.gif'
-import PI3 from './EmConstrução.gif'
+import PI3 from './PI3.gif'
 
 export default { PI1, PI2, PI3 }
